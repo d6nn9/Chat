71cdb3c0058060ded23129f2ef4fcec12e24855e
Danil Ryabkov <denr199@gmail.com>
2021-09-21 22:38:05 +0300

    include script chat

--- chat/public/script.js
+++ chat/public/script.js
@@ -1,36 +1,32 @@
-// window.addEventListener('DOMContentLoaded', function () {
-//   const form = document.getElementById('btn btn-primary regist');
+const socket = io();
 
-//   if (!form) return;
+function showStatus(status, message) {
+  document.querySelector('[data-status]').innerHTML = message || status;
+  document.querySelector('[data-status]').setAttribute('data-status', status);
+}
 
-//   form.onclick = function (event) {
-//     event.preventDefault();
+socket
+  .on('error', function (message) {
+    console.error(message);
+    showStatus('error', message);
+  });
 
-//     fetch("/registrated", {
-//       method: "POST",
-//       credentials: "include", // "omit" by default, for cookies to work
-//       headers: {
-//         'content-type': 'application/json'
-//       },
-//       body: JSON.stringify({
-//         email: this.email.value,
-//         password: this.password.value,
-//       })
-//     })
-//       .then(response => response.json())
-//       .then(response => {
-//         if (response.error) {
-//           alert(response.error.message);
-//         } else if (response.displayName) {
-//           // alert("WWWWWWWWWWWWWWWWWWWWWWWWWWW, " + response.displayName);
-//           window.location.reload(true);
-//         } else {
-//           throw new Error("Invalid response from the server");
-//         }
-//       })
-//       .catch(function (err) {
-//         alert("Error: " + err.message);
-//       });
-//   };
+'connect disconnect reconnect reconnecting reconnect_failed'.split(' ').forEach(function (event) {
+  socket.on(event, function () {
+    showStatus(event);
+  })
+});
 
-// });
+socket.on('logout', function () {
+  socket.disconnect();
+  window.location.reload();
+});
+
+socket.emit('test', 'test', (m) => {
+  console.log('from server', m);
+});
+
+socket.on('message', function (message, cb) {
+  console.log(message);
+  if (cb) cb("from client");
+});
